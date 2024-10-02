e182d4850b0b61e288825b705fc99361423bb08e
AdolfGimenez <adolfogimenez1093@gmail.com>
2024-10-02 18:35:25 -0400

    Add files via upload

---
+++ RRHH.sql
@@ -0,0 +1,16 @@
+select * from contabilid.TATSEDE
+select * from contabilid.FICLIEN
+select * from contabilid.SJMOVIM
+select * from contabilid.TATRAEM
+select * from CONTABILID.SJDPERS
+select * from contabilid.SJSBMES
+
+--tabla seguridad rr.hh
+SELECT * FROM GXBDCON.GXLOFUN G WHERE FUNNOM='sjmovim';--funcion
+SELECT * FROM GXBDCON.GXLOUSU G;--usuario
+SELECT * FROM GXBDCON.GXLOPER G WHERE USUCOD='821';--operador
+SELECT * FROM GXBDCON.GXLOPERDET G2 WHERE USUCOD='821';--detall
+
+CREATE TABLE PUBLIC.bkrhemple as
+select * from gth.rhemple where functiphor = 0;
+update gth.rhemple set functiphor = 1 where functiphor = 0;
